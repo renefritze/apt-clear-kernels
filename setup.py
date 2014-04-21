46f937d4d6a1c8bd8392ae1d8310db6c62407c07
Rene Milk <rene.milk@uni-muenster.de>
2014-04-21 18:31:52 +0200

    prototype setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='apt-clear-kernels',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='BSD 2-clause',
+    author='Rene Milk',
+    author_email='rene@milk.pm',
+    description='programmatically remove all but newest N kernels with apt',
+    install_requires=['python-apt'],
+    scripts=['apt-clear-kernels']
+)
